3cce2c75ce8af18b6c8813dbeaebf0dd1639675b
Hyunseung Yoo <hyunseung.yoo.hs@gmail.com>
2026-02-19 16:36:52 +0900

    Update solve_trap_model.py

--- solve_trap_model.py
+++ solve_trap_model.py
@@ -24,6 +24,8 @@ class GRID:
     q = 1.60217663e-19      # electron charge, [C]
     kb = 1.380649e-23       # Boltzmann constant, [m]^2 [kg] [K]^-1 [s]^-2 
     ep0 = 8.854187e-12      # elelctric permittivity of free space, [F] [m]^-1
+    h = 6.62607015e-34      # Planck constant, [m]^2 [kg] [s]^-1
+    hbar = h / (2.0*np.pi)
 
     def __init__(self):
         # grids
@@ -33,6 +35,7 @@ class GRID:
         self.K  = []    # dielectric constant (element)
         self.Q  = []    # charging flag (node)
         self.T  = []    # charging density (node)
+        self.TT = []    # TOX tunneling flag (node)
         self.CB = []    # conduction band barrier height (element)
         self.VB = []    # valence band barrier height (element)
         self.CE = []    # conduction band effective mass (element)
@@ -55,7 +58,7 @@ class GRID:
         self.CH_E_AFF = ch_e_affinity       # c-silicon channel electron affinity = 4.05 eV
         self.WL_WF = wl_workfunction        # ALD TiN workfunction = 4.65 eV
 
-    def add_dielectric_layer(self, mat_name, thk, dt, trap):
+    def add_dielectric_layer(self, mat_name, thk, dt, trap, tox):
         # user input
         if self.G == {}:
             # starting from wordline side
@@ -64,21 +67,24 @@ class GRID:
             self.G['T']   = [thk]               # thickness in angstrom
             self.G['dT']  = [dt]                # resolution in angstrom
             self.G['Q']   = [trap]              # trapping flag
+            self.G['TT']  = [tox]               # TOX tunneling flag
         else:
             #
             self.G['MAT'].append(mat_name)
             self.G['T'].append(thk)
             self.G['dT'].append(dt)
             self.G['Q'].append(trap)
+            self.G['TT'].append(tox)
 
-    def make_unit_cell_structure(self):
+    def make_unit_cell_structure(self, tox_n_profile):
         # starting from channel side
         uc_t   = self.G['T'][::-1]
         uc_dt  = self.G['dT'][::-1]
         uc_mat = self.G['MAT'][::-1]
         uc_q   = self.G['Q'][::-1]
+        uc_tt  = self.G['TT'][::-1]
         
-        # making R, Q, T (node)
+        # making R, Q, T, TT (node)
         for index, each_t in enumerate(uc_t):
             #
             start_r = self.G['WL'] - np.sum(uc_t[index:])
@@ -87,19 +93,29 @@ class GRID:
             r_array = list(np.arange(start_r, end_r, uc_dt[index]))
             #
             if uc_q[index] == True:
-                q_array = [uc_q[index]] * (elmts + 1)
-                t_array = [1.0] * (elmts + 1)
+                q_array  = [uc_q[index]] * (elmts + 1)
+                t_array  = [1.0] * (elmts + 1)
+                tt_array = [uc_tt[index]] * elmts
+            elif uc_tt[index] == True:
+                q_array  = [uc_q[index]] * elmts
+                t_array  = [0.0] * elmts
+                tt_array = [uc_tt[index]] * (elmts + 1)
             else:
                 q_array = [uc_q[index]] * elmts
                 t_array = [0.0] * elmts
+                tt_array = [uc_tt[index]] * elmts
             #
-            self.R += r_array
-            self.Q += q_array
-            self.T += t_array
+            self.R  += r_array
+            self.Q  += q_array
+            self.T  += t_array
+            self.TT += tt_array
         # 
         self.R += [self.G['WL']]
         #
         self.R = np.array(self.R)
+
+        for index in range(len(self.R)):
+            print(index, self.R[index], self.Q[index], self.T[index], self.TT[index])
         
         # making M (element)
         for index, each_mat in enumerate(uc_mat):
@@ -117,6 +133,28 @@ class GRID:
             self.VB.append(self.MAT[each_mat]['vb_bh'])
             self.CE.append(self.MAT[each_mat]['cb_meff'])
             self.VE.append(self.MAT[each_mat]['vb_meff'])
+
+        # TOX N profile (band engineering, linear interpolation)
+        uc_tox_n_peak_pos  = int(uc_t[0]*tox_n_profile['tox_peak']['pos'])
+        uc_tox_n_ch_side   = tox_n_profile['ch_side']['n']
+        uc_tox_n_peak      = tox_n_profile['tox_peak']['n']
+        uc_tox_n_ctn_side  = tox_n_profile['ctn_side']['n']
+        uc_tox_n_diff_1    = uc_tox_n_peak - uc_tox_n_ch_side
+        uc_tox_n_diff_2    = uc_tox_n_ctn_side - uc_tox_n_peak
+        uc_tox_pos = np.array( list( range( int(uc_t[0]/uc_dt[0]) ) ), dtype=int )
+        uc_tox_n   = np.zeros( int(uc_t[0]/uc_dt[0]) )
+        uc_tox_n   = np.where( uc_tox_pos <= uc_tox_n_peak_pos, \
+                               uc_tox_n_ch_side + uc_tox_n_diff_1 * uc_tox_pos / uc_tox_n_peak_pos, 0.0 )
+        uc_tox_n  += np.where( uc_tox_pos > uc_tox_n_peak_pos, \
+                               uc_tox_n_peak + uc_tox_n_diff_2 * (uc_tox_pos - uc_tox_n_peak_pos) / (uc_tox_pos[-1] - uc_tox_n_peak_pos), 0.0 ) 
+
+        for index in range( int(uc_t[0]/uc_dt[0]) ):
+            #
+            self.K[index]  = self.MAT['SIO2']['k']       * (1.0 - uc_tox_n[index]) + self.MAT['SI3N4']['k']       * uc_tox_n[index]
+            self.CB[index] = self.MAT['SIO2']['cb_bh']   * (1.0 - uc_tox_n[index]) + self.MAT['SI3N4']['cb_bh']   * uc_tox_n[index]
+            self.VB[index] = self.MAT['SIO2']['vb_bh']   * (1.0 - uc_tox_n[index]) + self.MAT['SI3N4']['vb_bh']   * uc_tox_n[index]
+            self.CE[index] = self.MAT['SIO2']['cb_meff'] * (1.0 - uc_tox_n[index]) + self.MAT['SI3N4']['cb_meff'] * uc_tox_n[index]
+            self.VE[index] = self.MAT['SIO2']['vb_meff'] * (1.0 - uc_tox_n[index]) + self.MAT['SI3N4']['vb_meff'] * uc_tox_n[index]
 
     def make_poission_matrix(self):
         #
@@ -230,6 +268,32 @@ class GRID:
 
 class SOLVER(GRID):
 
+    def calculate_vt_shift(self):
+        # starting from neutral state
+        self.VTS = 0.0
+        # check trap layer
+        for each_index in range(len(self.Q)):
+            # trap layer
+            if self.Q[each_index] == True:
+                # geometry 1
+                each_r = self.R[each_index] * 1e-10
+                each_dr = self.R[each_index+1] * 1e-10 - self.R[each_index] * 1e-10
+                # geometry 2
+                each_area = 2.0 * np.pi * each_r
+                each_volume = each_area * each_dr
+                # net charge
+                each_charge = self.q * self.T[each_index] * each_volume
+                # from net charge to wordline capacitance inverse
+                k_array  = np.array(self.K[each_index:])
+                r_array  = np.array(self.R[each_index:-1])
+                dr_array = np.array(self.R[(each_index+1):]) - np.array(self.R[each_index:-1])
+                c_inv_array = dr_array / (self.ep0 * k_array) / (2.0 * np.pi * r_array)
+                C_inv = np.sum(c_inv_array)
+                # accumulated charge on channel: dV = dQ * C inverse
+                self.VTS += -each_charge * C_inv
+        #
+        return self.VTS
+
     def solve_poission_equation(self, ch_bias, wl_bias):
         # updating external bias vector
         self.EB[0]  = ch_bias
@@ -249,25 +313,33 @@ class SOLVER(GRID):
 mat_para_dic = {}
 mat_para_dic['SIO2']  = {'no':10, 'k':3.9,  'eg':8.9, 'cb_bh':3.0, 'vb_bh':4.4, 'cb_meff':0.50, 'vb_meff':0.60}
 mat_para_dic['SI3N4'] = {'no':11, 'k':7.5,  'eg':5.0, 'cb_bh':2.0, 'vb_bh':1.5, 'cb_meff':0.24, 'vb_meff':0.30} 
-mat_para_dic['AL2O3'] = {'no':12, 'k':9.0,  'eg':8.7, 'cb_bh':2.7, 'vb_bh':4.8, 'cb_meff':0.35, 'vb_meff':0.45}
+mat_para_dic['AL2O3'] = {'no':12, 'k':9.0,  'eg':8.7, 'cb_bh':2.7, 'vb_bh':4.8, 'cb_meff':0.40, 'vb_meff':0.45}
 mat_para_dic['HFO2']  = {'no':13, 'k':23.0, 'eg':4.5, 'cb_bh':1.5, 'vb_bh':1.9, 'cb_meff':0.17, 'vb_meff':0.22}
 mat_para_dic['ZRO2']  = {'no':14, 'k':18.0, 'eg':5.0, 'cb_bh':1.4, 'vb_bh':2.2, 'cb_meff':0.15, 'vb_meff':0.25}
 
+tox_n_profile = {}
+tox_n_profile['ch_side']  = {'n':0.05, 'pos':0.00}      # portion
+tox_n_profile['tox_peak'] = {'n':0.25, 'pos':0.50}      # portion
+tox_n_profile['ctn_side'] = {'n':0.15, 'pos':1.00}      # portion
+
 grid_solver = SOLVER()
 grid_solver.add_material_parameters(mat_para_dic=mat_para_dic)
-grid_solver.set_plug_cd(plug_cd=1200)                                               # in angstrom
-grid_solver.set_channel_wordline_para(ch_e_affinity=4.05, wl_workfunction=4.65)     # in eV
-grid_solver.add_dielectric_layer(mat_name='AL2O3', thk=25.0, dt=1.0, trap=False)    # in angstrom, BOX
-grid_solver.add_dielectric_layer(mat_name='SIO2',  thk=68.0, dt=1.0, trap=False)    # in angstrom, BOX
-grid_solver.add_dielectric_layer(mat_name='SI3N4', thk=44.0, dt=1.0, trap=True)     # in angstrom, CTN
-grid_solver.add_dielectric_layer(mat_name='SIO2',  thk=49.0, dt=1.0, trap=False)    # in angstrom, TOX
-grid_solver.make_unit_cell_structure()
+grid_solver.set_plug_cd(plug_cd=1200)                                                           # in angstrom
+grid_solver.set_channel_wordline_para(ch_e_affinity=4.05, wl_workfunction=4.65)                 # in eV
+grid_solver.add_dielectric_layer(mat_name='AL2O3', thk=25.0, dt=1.0, trap=False, tox=False)     # in angstrom, BOX
+grid_solver.add_dielectric_layer(mat_name='SIO2',  thk=68.0, dt=1.0, trap=False, tox=False)     # in angstrom, BOX
+grid_solver.add_dielectric_layer(mat_name='SI3N4', thk=44.0, dt=1.0, trap=True,  tox=False)     # in angstrom, CTN
+grid_solver.add_dielectric_layer(mat_name='SIO2',  thk=49.0, dt=1.0, trap=False, tox=True)      # in angstrom, TOX
+grid_solver.make_unit_cell_structure(tox_n_profile=tox_n_profile)
 grid_solver.display_grid_info()
 
 grid_solver.make_poission_matrix()
-grid_solver.set_trap_density(trap_density=+1.0e25)
-grid_solver.solve_poission_equation(ch_bias=0.0, wl_bias=10.0)
+grid_solver.set_trap_density(trap_density=+0.6e25)
+vt_shift = grid_solver.calculate_vt_shift()
+grid_solver.solve_poission_equation(ch_bias=0.0, wl_bias=14.0)
+
 grid_solver.display_band_diagram(output_filename='ctn_trap_model_test.pdf')
+
 
 
 
